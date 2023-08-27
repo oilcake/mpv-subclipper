a561c7283919e9d0ef797417c48262c12fd58ff7
oilcake <oilpie@gmail.com>
2023-08-27 17:02:57 +0300

    hotkeys updated

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ mp.add_key_binding("g", "prev", looper.prev_loop)
 mp.add_key_binding("h", "next", looper.next_loop)
 mp.add_key_binding("(", "right", looper.insert_right)
 mp.add_key_binding(")", "left", looper.insert_left)
-mp.add_key_binding("?", "split", looper.split_at_play_position)
+mp.add_key_binding("k", "split", looper.split_at_play_position)
 
 --[[
 mp.add_key_binding("§", "save", add your function here)
