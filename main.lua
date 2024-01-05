cfbcd419d2de80d08c9a92eed87dbd6cd7b6879e
oilcake <oilpie@gmail.com>
2024-01-05 14:14:43 +0300

    shortcuts changed

--- main.lua
+++ main.lua
@@ -3,10 +3,10 @@ local looper = require("subcliper")
 
 mp.register_event("file-loaded", looper.init)
 
-mp.add_key_binding("-", "start", looper.loop_start)
-mp.add_key_binding("=", "end", looper.loop_end)
+mp.add_key_binding("^", "start", looper.loop_start)
+mp.add_key_binding("&", "end", looper.loop_end)
 mp.add_key_binding("+", "new", looper.loop_add)
-mp.add_key_binding("_", "remove", looper.loop_drop)
+mp.add_key_binding("H", "remove", looper.loop_drop)
 mp.add_key_binding("g", "prev", looper.prev_loop)
 mp.add_key_binding("h", "next", looper.next_loop)
 mp.add_key_binding("(", "right", looper.insert_right)
