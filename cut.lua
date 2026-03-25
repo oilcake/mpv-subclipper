9debecdb568791d2761a1191e76dddac37d3e0d1
oilcake <oilpie@gmail.com>
2026-03-25 14:57:30 +0300

    formatted

--- cut.lua
+++ cut.lua
@@ -12,181 +12,181 @@ local PRORES_TRANSCODE = "-c:v prores_ks -profile:v 0"
 local PRORES_CONTAINER = "mov"
 local MP4_CONTAINER = "mp4"
 local FFPROBE =
-"ffprobe -v error -hide_banner -of default=noprint_wrappers=0 -print_format flat -select_streams v:0 -show_entries stream=width,height,codec_type "
+	"ffprobe -v error -hide_banner -of default=noprint_wrappers=0 -print_format flat -select_streams v:0 -show_entries stream=width,height,codec_type "
 local SMARTCUT = "smartcut"
 local KEEP = "--keep"
 local COMMA = ","
 
 -- main object that handles clip info and runs conversion process
 local HandSaw = {
-  file = nil,
-  container_from = nil,
-  container_to = nil,
-  width = 0,
-  height = 0,
-  scaled_height = 0,
-  output = nil,
-  name_prefix = "",
-  edges = nil,    -- loop object
-  clip_name = nil,
-  clip_path = nil, -- full path to clip
-  what_to_do = COPY,
-  downscale_options = "",
-  valid_video = false,
-  exit_status = {
-    ok = nil,
-    status = nil,
-    code = nil,
-  },
+	file = nil,
+	container_from = nil,
+	container_to = nil,
+	width = 0,
+	height = 0,
+	scaled_height = 0,
+	output = nil,
+	name_prefix = "",
+	edges = nil, -- loop object
+	clip_name = nil,
+	clip_path = nil, -- full path to clip
+	what_to_do = COPY,
+	downscale_options = "",
+	valid_video = false,
+	exit_status = {
+		ok = nil,
+		status = nil,
+		code = nil,
+	},
 }
 
 -- new instance
 function HandSaw:new(file, output_location)
-  setmetatable({}, self)
-  self.__index = self
-  self.file = file
-  local file_location, name, type = path.strip_path(file)
-  local parent_dir = path.strip_parent_dir(file_location)
-  if output_location ~= nil then
-    -- output_location = path.join({output_location, parent_dir})
-    output_location = output_location .. "/" .. parent_dir
-  end
-  file_location = "/" .. file_location
-  self.output_dir = path.join({ output_location or file_location, name })
-  if not output_location then
-    io.write("saving into default location, cause no location was specified\n")
-  end
-  path.create_dir_from(self.output_dir)
-  self.container_from = type
-  self.name_prefix = name
-  self:get_info()
-  self.scaled_height = self.height
-  return self
+	setmetatable({}, self)
+	self.__index = self
+	self.file = file
+	local file_location, name, type = path.strip_path(file)
+	local parent_dir = path.strip_parent_dir(file_location)
+	if output_location ~= nil then
+		-- output_location = path.join({output_location, parent_dir})
+		output_location = output_location .. "/" .. parent_dir
+	end
+	file_location = "/" .. file_location
+	self.output_dir = path.join({ output_location or file_location, name })
+	if not output_location then
+		io.write("saving into default location, cause no location was specified\n")
+	end
+	path.create_dir_from(self.output_dir)
+	self.container_from = type
+	self.name_prefix = name
+	self:get_info()
+	self.scaled_height = self.height
+	return self
 end
 
 function HandSaw:get_info()
-  -- this method gets clip dimensions from ffprobe
-  -- and checks if video file is valid
-  local response = io.popen(FFPROBE .. path.escape_shell(self.file))
-  if response ~= nil then
-    local ffprobe = response:read("*a")
-    response:close()
-    self.width = tonumber(string.match(ffprobe, ".+width=(%d+)"))
-    self.height = tonumber(string.match(ffprobe, ".+height=(%d+)"))
-    local codec_type = string.match(ffprobe, [[.+codec_type="(.+)"]])
-    if codec_type == "video" then
-      self.valid_video = true
-    end
-  end
+	-- this method gets clip dimensions from ffprobe
+	-- and checks if video file is valid
+	local response = io.popen(FFPROBE .. path.escape_shell(self.file))
+	if response ~= nil then
+		local ffprobe = response:read("*a")
+		response:close()
+		self.width = tonumber(string.match(ffprobe, ".+width=(%d+)"))
+		self.height = tonumber(string.match(ffprobe, ".+height=(%d+)"))
+		local codec_type = string.match(ffprobe, [[.+codec_type="(.+)"]])
+		if codec_type == "video" then
+			self.valid_video = true
+		end
+	end
 end
 
 function HandSaw:define_region(loop)
-  -- region of video to cut
-  self.edges = loop
+	-- region of video to cut
+	self.edges = loop
 end
 
 -- format all needed args
 function HandSaw:format_args()
-  self.clip_name = string.format("%s-%.2f-%.2f.%s", self.name_prefix, self.edges.a, self.edges.b, self.container_to)
-  self.clip_path = path.join({ self.output_dir, self.clip_name })
-  local args = string.format(
-    "%s",
-    FFMPEG
-    .. SPACE
-    .. FROM
-    .. SPACE
-    .. self.edges.a
-    .. SPACE
-    .. TO
-    .. SPACE
-    .. self.edges.b
-    .. SPACE
-    .. DO_NOT_OVERWRITE
-    .. SPACE
-    .. INPUT
-    .. SPACE
-    .. path.escape_shell(self.file)
-    .. SPACE
-    .. self.what_to_do
-    .. SPACE
-    .. path.escape_shell(self.clip_path)
-  )
-  return args
+	self.clip_name = string.format("%s-%.2f-%.2f.%s", self.name_prefix, self.edges.a, self.edges.b, self.container_to)
+	self.clip_path = path.join({ self.output_dir, self.clip_name })
+	local args = string.format(
+		"%s",
+		FFMPEG
+			.. SPACE
+			.. FROM
+			.. SPACE
+			.. self.edges.a
+			.. SPACE
+			.. TO
+			.. SPACE
+			.. self.edges.b
+			.. SPACE
+			.. DO_NOT_OVERWRITE
+			.. SPACE
+			.. INPUT
+			.. SPACE
+			.. path.escape_shell(self.file)
+			.. SPACE
+			.. self.what_to_do
+			.. SPACE
+			.. path.escape_shell(self.clip_path)
+	)
+	return args
 end
 
 function HandSaw:do_thing()
-  -- run ffmpeg and return results from the shell
-  local args = self:format_args()
-  self.exit_status.ok, self.exit_status.status, self.exit_status.code = os.execute(args)
-  return self.exit_status
+	-- run ffmpeg and return results from the shell
+	local args = self:format_args()
+	self.exit_status.ok, self.exit_status.status, self.exit_status.code = os.execute(args)
+	return self.exit_status
 end
 
 -- helper function
 local function downscale_string(height)
-  return string.format("-filter:v scale=-1:%d", height)
+	return string.format("-filter:v scale=-1:%d", height)
 end
 
 --[[CONVERSION METHODS]]
 --
 function HandSaw:copy_clip()
-  -- tries to save a lossless copy of video's section
-  self.container_to = self.container_from
-  self.clip_name = string.format("%s-%.2f-%.2f.%s", self.name_prefix, self.edges.a, self.edges.b, self.container_to)
-  self.clip_path = path.join({ self.output_dir, self.clip_name })
-  if path.file_exists(self.clip_path) then
-    self.exit_status.ok = false
-    self.exit_status.status = nil
-    self.exit_status.code = 1
-    return self.exit_status
-  end
-  local args = string.format(
-    "%s",
-    SMARTCUT
-    .. SPACE
-    .. path.escape_shell(self.file)
-    .. SPACE
-    .. path.escape_shell(self.clip_path)
-    .. SPACE
-    .. KEEP
-    .. SPACE
-    .. self.edges.a
-    .. COMMA
-    .. self.edges.b
-  )
-  print(string.format("copying with args: %s", args))
-  self.exit_status.ok, self.exit_status.status, self.exit_status.code = os.execute(args)
-  return self.exit_status
-  -- returns shell's exit code
+	-- tries to save a lossless copy of video's section
+	self.container_to = self.container_from
+	self.clip_name = string.format("%s-%.2f-%.2f.%s", self.name_prefix, self.edges.a, self.edges.b, self.container_to)
+	self.clip_path = path.join({ self.output_dir, self.clip_name })
+	if path.file_exists(self.clip_path) then
+		self.exit_status.ok = false
+		self.exit_status.status = nil
+		self.exit_status.code = 1
+		return self.exit_status
+	end
+	local args = string.format(
+		"%s",
+		SMARTCUT
+			.. SPACE
+			.. path.escape_shell(self.file)
+			.. SPACE
+			.. path.escape_shell(self.clip_path)
+			.. SPACE
+			.. KEEP
+			.. SPACE
+			.. self.edges.a
+			.. COMMA
+			.. self.edges.b
+	)
+	print(string.format("copying with args: %s", args))
+	self.exit_status.ok, self.exit_status.status, self.exit_status.code = os.execute(args)
+	return self.exit_status
+	-- returns shell's exit code
 end
 
 function HandSaw:transcode_to_prores()
-  self.what_to_do = PRORES_TRANSCODE
-  self.container_to = PRORES_CONTAINER
-  return self:do_thing()
+	self.what_to_do = PRORES_TRANSCODE
+	self.container_to = PRORES_CONTAINER
+	return self:do_thing()
 end
 
 function HandSaw:transcode_to_hq_mp4()
-  self.what_to_do = CRF_COPY
-  self.container_to = MP4_CONTAINER
-  return self:do_thing()
+	self.what_to_do = CRF_COPY
+	self.container_to = MP4_CONTAINER
+	return self:do_thing()
 end
 
 function HandSaw:transcode_to_mp4()
-  self.what_to_do = ""
-  self.container_to = MP4_CONTAINER
-  return self:do_thing()
+	self.what_to_do = ""
+	self.container_to = MP4_CONTAINER
+	return self:do_thing()
 end
 
 function HandSaw:downscale_to_mp4(height)
-  self.what_to_do = downscale_string(height)
-  self.container_to = MP4_CONTAINER
-  return self:do_thing()
+	self.what_to_do = downscale_string(height)
+	self.container_to = MP4_CONTAINER
+	return self:do_thing()
 end
 
 function HandSaw:downscale_to_prores(height)
-  self.what_to_do = PRORES_TRANSCODE .. SPACE .. downscale_string(height)
-  self.container_to = PRORES_CONTAINER
-  return self:do_thing()
+	self.what_to_do = PRORES_TRANSCODE .. SPACE .. downscale_string(height)
+	self.container_to = PRORES_CONTAINER
+	return self:do_thing()
 end
 
 return HandSaw
--- path.lua
+++ path.lua
@@ -1,89 +1,89 @@
 local M = {}
 
 function M.escape_shell(s)
-  -- only for paths and names
-  -- do not use it if you want to pass literal "&" or "&&" to your shell
-  return (s:gsub("([ %(%)%\\%[%]'\"&#])", "\\%1"))
+	-- only for paths and names
+	-- do not use it if you want to pass literal "&" or "&&" to your shell
+	return (s:gsub("([ %(%)%\\%[%]'\"&#])", "\\%1"))
 end
 
 function M.strip_path(path_to_file)
-  -- returns path, filename, extension
-  local path = path_to_file:match("^/?(.+)/")
-  local name = path_to_file:match(".+/(.+)%..+$")
-  local type = path_to_file:match(".+%.(.+)$")
-  return path, name, type
+	-- returns path, filename, extension
+	local path = path_to_file:match("^/?(.+)/")
+	local name = path_to_file:match(".+/(.+)%..+$")
+	local type = path_to_file:match(".+%.(.+)$")
+	return path, name, type
 end
 
 function M.strip_parent_dir(path)
-  return path:match("^.+/(.+)$")
+	return path:match("^.+/(.+)$")
 end
 
 function M.join(parts)
-  -- joins parts of the full path, substitution of python's path.join
-  local slash = "/"
-  local joined = ""
-  joined = table.concat(parts, slash)
-  return joined
+	-- joins parts of the full path, substitution of python's path.join
+	local slash = "/"
+	local joined = ""
+	joined = table.concat(parts, slash)
+	return joined
 end
 
 function M.exists(file)
-  --- checks if a file or directory exists in this path
-  local ok, err, code = os.rename(file, file)
-  if not ok then
-    if code == 13 then
-      -- permission denied, but it exists
-      return true
-    end
-  end
-  return ok, err
+	--- checks if a file or directory exists in this path
+	local ok, err, code = os.rename(file, file)
+	if not ok then
+		if code == 13 then
+			-- permission denied, but it exists
+			return true
+		end
+	end
+	return ok, err
 end
 
 function M.isdir(path)
-  -- checks if a directory exists in this path
-  -- "/" works on both Unix and Windows
-  return M.exists(path .. "/")
+	-- checks if a directory exists in this path
+	-- "/" works on both Unix and Windows
+	return M.exists(path .. "/")
 end
 
 function M.listdir(dir)
-  -- opens directory looks for files
-  -- accepts unescaped path
-  local files = {}
-  local p = io.popen('find "' .. dir .. '" -type f')
-  -- loop through all files
-  if p == nil then
-    return nil, error("couldn't read dir")
-  end
-  for file in p:lines() do
-    if not file:match("^.+/%..+") then
-      table.insert(files, file)
-    end
-  end
-  return files, nil
+	-- opens directory looks for files
+	-- accepts unescaped path
+	local files = {}
+	local p = io.popen('find "' .. dir .. '" -type f')
+	-- loop through all files
+	if p == nil then
+		return nil, error("couldn't read dir")
+	end
+	for file in p:lines() do
+		if not file:match("^.+/%..+") then
+			table.insert(files, file)
+		end
+	end
+	return files, nil
 end
 
 function M.file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 function M.create_dir_from(name)
-  if not M.isdir(name) then
-    os.execute("mkdir -p " .. M.escape_shell(name))
-  end
+	if not M.isdir(name) then
+		os.execute("mkdir -p " .. M.escape_shell(name))
+	end
 end
 
 function M.move(file, new_path)
-  -- moves file to new location
-  local _, name, ext = M.strip_path(file)
-  local full_name = name .. "." .. ext
-  M.create_dir_from(new_path)
-  local moved = M.join({ new_path, full_name })
-  os.rename(file, moved)
+	-- moves file to new location
+	local _, name, ext = M.strip_path(file)
+	local full_name = name .. "." .. ext
+	M.create_dir_from(new_path)
+	local moved = M.join({ new_path, full_name })
+	os.rename(file, moved)
 end
 
 return M
